3586ea3913d3bb14ccad565e05822afef2064af5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 13:52:55 +0100

    Niedziela 2

--- th_app.py
+++ th_app.py
@@ -1,7 +1,10 @@
 def fizzbuzz(number):
-    if  number%3 == 0:
+    if number <= 0:
+        return None
+    elif number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif  number % 3 == 0:
         return "Fizz"
     elif numer % 5 == 0:
         return "Buzz"
-
     return number
