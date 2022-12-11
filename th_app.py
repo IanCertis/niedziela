5111909b706f681d32f3e7814c61d9479f476557
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 13:28:46 +0100

    nidziela

---
+++ th_app.py
@@ -0,0 +1,7 @@
+def fizzbuzz(number):
+    if  number%3 == 0:
+        return "Fizz"
+    elif numer % 5 == 0:
+        return "Buzz"
+
+    return number
---
+++ the_test.py
@@ -0,0 +1,9 @@
+from th_app import *
+
+def test_0():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(4) == 4
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
